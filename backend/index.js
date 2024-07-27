229472e1012dea252e5255907bd2ae961753d665
ujjwalukstar India <ujjwalkumar94310@gmail.com>
2024-07-27 21:57:41 +0530

    backend render url updated

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config();
 // connect db
 connectDB();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 
--- frontend/src/components/auth/Singup.jsx
+++ frontend/src/components/auth/Singup.jsx
@@ -47,7 +47,7 @@ const Signup = () => {
 
     try {
       dispatch(setLoading(true)); // Start loading
-      const res = await axios.post("http://localhost:8000/api/v1/user/register", formData, {
+      const res = await axios.post("https://getxhired.onrender.com/api/v1/user/register", formData, {
         headers: {
           "Content-Type": "multipart/form-data"
         },
