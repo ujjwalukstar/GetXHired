1d468e1aeb71fc29734441abafe9632102ea9bb6
ujjwalukstar India <ujjwalkumar94310@gmail.com>
2024-07-27 22:27:03 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://getxhired-frontend.onrender.com/",
     credentials:true
 }
 app.use(cors(corsOptions));
